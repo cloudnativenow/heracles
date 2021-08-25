d54ae7bfb26a83ffeff1ccd14d15fa9f66ffb6b8
Anthony Angelo <anthonyangelo@yahoo.com>
2021-08-25 16:50:08 -0400

    Update

--- modules/heracles/02-amis.tf
+++ modules/heracles/02-amis.tf
@@ -1,11 +1,15 @@
 # Define an Amazon Linux AMI.
-data "aws_ami" "amazonlinux" {
+data "aws_ami" "amazon-linux-2" {
   most_recent = true
-  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
 
   filter {
     name   = "name"
-    values = ["amzn-ami-hvm*"]
+    values = ["amzn2-ami-hvm*"]
   }
 
   filter {
