fe6ded1c9f164c04de6270ff8a78232cb214dbcd
Anthony Angelo <anthonyangelo@yahoo.com>
2021-09-07 23:11:12 -0400

    Update

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 # Save Terraform State to S3 Bucket
 terraform {
   backend "s3" {
-    # bucket = "hlawork99-terraform-backend"
-    bucket = "hlawork1-terraform-backend"
+    bucket = "hlawork99-terraform-backend"
+    # bucket = "hlawork1-terraform-backend"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
