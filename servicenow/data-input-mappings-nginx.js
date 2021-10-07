579d5fffdaed6f331ff445d5be04899eb6bbb2e9
Anthony Angelo <anthonyangelo@yahoo.com>
2021-10-07 08:52:35 -0400

    Update

---
+++ servicenow/data-input-mappings-nginx.js
@@ -0,0 +1,8 @@
+function map(sample, metadata) {
+    return {
+      'applicationService': 'Pet Clinic 20210916-1',
+      'component': null,
+      'sourceType': null,
+    };
+  }
+  // Do not write code here
