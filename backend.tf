708d45d0ded605028326898a95e3ae9f23a6b1b9
Anthony Angelo <anthonyangelo@yahoo.com>
2021-10-25 22:59:18 -0400

    Update

--- backend.tf
+++ backend.tf
@@ -3,7 +3,9 @@ terraform {
   backend "s3" {
     # bucket = "hlawork1-terraform-backend"
     # bucket = "hlawork2-terraform-backend"
-    bucket = "hlawork3-terraform-backend"
+    # bucket = "hlawork3-terraform-backend"
+    # bucket = "hlawork4-terraform-backend"
+    bucket = "hlawork5-terraform-backend"
     # bucket = "hlawork99-terraform-backend"    
     key    = "terraform.tfstate"
     region = "us-east-2"
