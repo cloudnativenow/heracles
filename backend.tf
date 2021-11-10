62a42e212e6c82fa2f483fb3dd28a220345b411a
Anthony Angelo <anthonyangelo@yahoo.com>
2021-11-10 08:11:31 -0500

    Safe zone

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
     # bucket = "hlawork2-terraform-backend"
     # bucket = "hlawork3-terraform-backend"
     # bucket = "hlawork4-terraform-backend"
-    bucket = "hlawork5-terraform-backend"
+    # bucket = "hlawork5-terraform-backend"
     # bucket = "hlawork99-terraform-backend"    
     key    = "terraform.tfstate"
     region = "us-east-2"
