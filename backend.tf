03674f0e0bd009415eff51366bceb1a34cbe0e3e
Netser Heruty <netsertser@gmail.com>
2022-02-07 17:45:28 +0200

    added update set for acc log policices

--- backend.tf
+++ backend.tf
@@ -1,12 +1,7 @@
 # Save Terraform State to S3 Bucket
 terraform {
   backend "s3" {
-    # bucket = "hlawork1-terraform-backend"
-    # bucket = "hlawork2-terraform-backend"
-    # bucket = "hlawork3-terraform-backend"
-    # bucket = "hlawork4-terraform-backend"
-    # bucket = "hlawork5-terraform-backend"
-    bucket = "hlawork99-terraform-backend"    
+    bucket = "nhwork2-terraform-backend"    
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
