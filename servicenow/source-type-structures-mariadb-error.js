6a7073bfa71317e051b4e1f8a54499ec435ba568
Anthony Angelo <anthonyangelo@yahoo.com>
2022-02-09 08:56:25 -0500

    Reindex

--- servicenow/source-type-structures-mariadb-error.js
+++
@@ -1,8 +0,0 @@
-function construct(sample, output) {
-	var match = sample.match(/^([\d\-]+ +[\d:]+) (?:\d+ )?\[(\w+)\] (.+)/);
-	if (match != null && match.length > 0) {
-		output['timestamp'] = match[1].replace('  ',' 0');
-		output['severity'] = match[2].replace('Note','Notice');
-		output['message'] = match[3];
-	}
-}
--- servicenow/source-type-structures-syslog-messages.js
+++
@@ -1,6 +0,0 @@
-function construct(sample, output) {
-	if (sample.match(/: .+/)){
-		output['message'] = sample.match(/: (.+)/)[1];
-	}
-}
-// Do not write code here
