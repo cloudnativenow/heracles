b0613d4bcaf4d9fcebce85ddd6b59f67e5e346d0
Anthony Angelo <anthonyangelo@yahoo.com>
2022-02-16 20:45:10 -0500

    2.0 now uses latest terraform

--- releases/2.0/backend.tf
+++ releases/2.0/backend.tf
@@ -1,7 +1,7 @@
 # Save Terraform State to S3 Bucket
 terraform {
   backend "s3" {
-    bucket = "nhwork2-terraform-backend"    
+    bucket = "junk-terraform-backend"    
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
