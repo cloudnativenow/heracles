7e4876d1812018afc1f04a8dd7cdd34b04159879
longshot4 <rusinskimark@gmail.com>
2023-08-25 14:52:36 -0400

    Revert "Update 02-amis.tfm to use golden image"

--- releases/2.0/modules/heracles/02-amis.tf
+++ releases/2.0/modules/heracles/02-amis.tf
@@ -1,10 +1,20 @@
 # Define an Amazon Linux AMI.
 data "aws_ami" "amazonlinux" {
   most_recent = true
-  owners = ["565710867928"]
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ServiceNow_AmazonLinux2_Image_*"]
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
